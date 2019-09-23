85cb1e42bc62f71ff7867bc196547b12a08b9e3c
Josemar Moura <josemarsp@gmail.com>
2019-09-23 20:24:56 -0300

    Sem AppMenu no Splash

--- app/src/main/java/br/com/josef/movieaddiction/view/SplashActivity.java
+++ app/src/main/java/br/com/josef/movieaddiction/view/SplashActivity.java
@@ -16,10 +16,13 @@ public class SplashActivity extends AppCompatActivity {
     private ImageView imgSplash;
     private Timer timer = new Timer();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide(); // tira a AppBar
 
         imgSplash = findViewById(R.id.img_splash_id);
 
