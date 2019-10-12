99dbfaa32a79df25a7a30a1b3921a4bf2684fce8
Andreza Campbell <campbell.andreza@gmail.com>
2019-10-12 18:11:53 -0300

    acerto de layout da tela de filmes, acrescimos de atributos no pojo do filmes, acrescimo de atributos no fragmento java filmeResultado, add interface de filmes assistidos e nao assistidos

--- app/src/main/java/br/com/josef/movieaddiction/fragments/ListaDeFilmesNaoAssistidosFragment.java
+++ app/src/main/java/br/com/josef/movieaddiction/fragments/ListaDeFilmesNaoAssistidosFragment.java
@@ -6,5 +6,4 @@ import androidx.fragment.app.Fragment;
 
 public class ListaDeFilmesNaoAssistidosFragment extends Fragment {
 
-
 }
---
+++ app/src/main/java/br/com/josef/movieaddiction/interfaces/RVOnClickFilmesNaoAssistidos.java
@@ -0,0 +1,4 @@
+package br.com.josef.movieaddiction.interfaces;
+
+public interface RVOnclickFilmesNaoAssistidos {
+}
